2fb092055bd10be0c71b5db4e7b87fa1fea58ec3
Kantharaj <kantharajbhandary@gmail.com>
2025-03-18 11:21:05 -0700

    added

---
+++ projectOfMarch/src/com/adv/tourer/Rcb.java
@@ -0,0 +1,12 @@
+package com.adv.tourer;
+
+import javax.sound.midi.Soundbank;
+import javax.xml.bind.SchemaOutputResolver;
+
+public class Rcb {
+    void TeamMem(RcbManagement rcbManagement)
+    {
+        System.out.println("listed all players");
+    }
+
+}
---
+++ projectOfMarch/src/com/adv/tourer/RcbManagement.java
@@ -0,0 +1,14 @@
+package com.adv.tourer;
+
+
+
+public class RcbManagement {
+    void rcbTeam()
+    {
+        Rcb rcb=new Rcb();
+        RcbManagement rcbManagement=new RcbManagement();
+        rcb.TeamMem(rcbManagement);
+
+
+    }
+}
